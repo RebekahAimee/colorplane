db2cd94a1e1141d207717f09717b9651da0af8b2
Rebekah Aimee Yoder <rebekahaimeeyoder@gmail.com>
2017-03-13 15:01:29 -0500

    Add basic structure and imports to main.py

---
+++ main.py
@@ -0,0 +1,37 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.listview import ListView
+from kivy.storage.jsonstore import JsonStore
+
+import json
+
+# APP CLASS
+class ColorwallApp(App):
+    pass
+
+# MAIN SCREEN
+class Body():
+    def __init__(self, **kwargs):
+        # later
+        pass
+
+    def on_touch_down(self, touch):
+        # put marks down
+        pass
+
+# MAIN MENU
+class MainMenu(BoxLayout):
+    pass
+
+# SUBMENUS
+class PaletteSelectMenu(BoxLayout):
+    pass
+
+class ColorSelectMenu(BoxLayout):
+    pass
+
+class DonationMenu(BoxLayout):
+    pass
+
+
+
